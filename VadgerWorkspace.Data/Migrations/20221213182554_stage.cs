df0b89977da0fcadb20c7b81da2a4254f7906dd4
Ренат Ренат <renat.churbanov@gmail.com>
2022-12-13 21:55:07 +0300

    Added Getclient async function into repo added stage into client model

---
+++ VadgerWorkspace.Data/Migrations/20221213182554_stage.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VadgerWorkspace.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class stage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RegistrationStage",
+                table: "Clients",
+                newName: "Stage");
+
+            migrationBuilder.AddColumn<long>(
+                name: "ClientId",
+                table: "Employees",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "Stage",
+                table: "Clients",
+                newName: "RegistrationStage");
+        }
+    }
+}
